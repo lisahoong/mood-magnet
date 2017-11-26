74a612e5951a256e6a94928712d1b9dbdbd9d7b1
lisahoong <lhoong94@gmail.com>
2017-11-26 17:04:38 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Login from './Login.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>hi</h1>
+        <Login/>
       </div>
     );
   }
---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div className="login-container">
+        <img className="logo-login" src="/images/logo.jpg"/>
+          <div>Log in to get started!</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
